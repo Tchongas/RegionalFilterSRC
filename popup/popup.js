c74644df4e701e287c8882f3c11848bcc03ef41d
Tchongas <you@example.com>
2023-10-30 14:42:04 -0300

    removed old code

--- popup/popup.js
+++ popup/popup.js
@@ -23,9 +23,4 @@ document.addEventListener('DOMContentLoaded', function() {
       chrome.tabs.sendMessage(tabs[0].id, { action: 'undo_delete' });
     });
   });
-  insertButton.addEventListener('click', function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'insert_button' });
-    });
-  });
 });
